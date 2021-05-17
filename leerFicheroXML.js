46875f1425eccfda7b787665003843ed6e351540
marianodepa96 <76202425+marianodepa96@users.noreply.github.com>
2021-05-17 21:52:51 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -1,6 +1,7 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libro")
+	let librerias = xmlDoc.querySelectorALL("libreria")
 	for(let i=0; i<libros.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
 	
